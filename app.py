e1b745aa331311deb48403e181689371b9464649
Ryan Walkley <rewalkley2@gmail.com>
2021-05-12 23:44:38 -0500

    adding the HW files.

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask
 app =  Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello world'
-
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
